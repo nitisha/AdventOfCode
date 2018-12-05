0038692c7cd71ed5a38aa5e779bf1fd14d4f519c
Nitisha D <nitishad@Nitishas-MacBook-Pro.local>
2018-12-05 13:13:40 -0800

    second challenge for day1

--- Day1/day1_chronalcalibration.py
+++ Day1/day1_chronalcalibration.py
@@ -1,11 +1,22 @@
 def chronal_calibration():
     frequency = 0
-
     with open("day1_input.txt") as input_file:
         for line in input_file:
             frequency += int(line)
-
     return frequency
 
-frequency = chronal_calibration()
+
+def chronal_calibration_2():
+    past_frequencies = set()
+    frequency = 0
+    while True:
+        with open("day1_input.txt") as input_file:
+            for line in input_file:
+                frequency += int(line)
+                if frequency in past_frequencies:
+                    return frequency
+                past_frequencies.add(frequency)
+    return frequency
+
+frequency = chronal_calibration_2()
 print frequency, 'is the frequency'
